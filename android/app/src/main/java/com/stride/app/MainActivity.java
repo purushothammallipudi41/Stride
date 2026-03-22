0c4b095e430ab82d9e8f1c886dd415d2ef8a31bf
purushotham <purushothammalllipudi411@gmail.com>
2026-03-22 23:45:47 +0530

    Initialize Android native platform and update versioning to 1.0.1

---
+++ android/app/src/main/java/com/stride/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.stride.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Explore from './pages/Explore';
 import Messages from './pages/Messages';
 import Profile from './pages/Profile';
 import Notifications from './pages/Notifications';
-import More from './pages/More';
+
 import Settings from './pages/Settings';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -65,7 +65,7 @@ const AppContent = () => {
           <Route path="/messages" element={<Messages />} />
           <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
           <Route path="/notifications" element={isAuthenticated ? <Notifications /> : <Navigate to="/login" />} />
-          <Route path="/more" element={<More />} />
+
           <Route path="/settings" element={isAuthenticated ? <Settings /> : <Navigate to="/login" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
